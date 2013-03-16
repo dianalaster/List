0d6b4c1979dc8fff82c13eb3b4b16c5d5e30e96b
Diana Pechter <diana@handybook.com>
2013-03-15 22:28:31 -0400

    Create List Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+List::Application.config.secret_token = '3fa5d0dd4957abd0c15f94e690fa04f2285edc08531284193741ba095abca80f375c60d0bf28336b8f21e251c9b1466ccf90e70c32430c6bc91f2add4ab59b9a'
